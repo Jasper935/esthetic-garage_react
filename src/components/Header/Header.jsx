a006283e942ada42400ec94d3066beff1d7bba24
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-07 17:38:06 +0200

    u

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,37 @@
-import css from './Header.module.css'
-
-export const Header=()=>{
-
-    return(
-       <header className={css.header}>
-        
-       </header>
-    )
-}
+import css from './Header.module.css';
+import logo from '../../images/logo2.png';
+import svg from '../../images/symbol-defs.svg';
+export const Header = () => {
+  const socials = [
+    { id: 1, icon: `${svg}#icon-telegram`, link: 'instagram.com' },
+    { id: 2, icon: `${svg}#icon-instagram`, link: 'instagram.com' },
+    { id: 2, icon: `${svg}#icon-facebook`, link: 'instagram.com' },
+    { id: 2, icon: `${svg}#icon-instagram`, link: 'instagram.com' },
+  ];
+  return (
+    <header className={css.header}>
+      <div className={css.wrap}>
+        <ul className={css.socialsList}>
+            {/* <div className={css.sociaBackground}></div> */}
+          {socials.map(({ id, icon, link }) => {
+            return (
+              <li className={css.socialsItem} key={id}>
+                <a className={css.socialsLink} href={link} target={'_blank'} rel="noreferrer">
+                  <svg className={css.socialsIcon} >
+                    <use href={icon} />
+                  </svg>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <img className={css.logo} src={logo} alt="" />
+        <button class={css.burgerBtn}>
+          <svg className={css.burger}>
+            <use href={`${svg}#icon-menu`} />
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+};
