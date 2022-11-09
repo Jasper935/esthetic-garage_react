515c7800eefa0b37ebeb271d9863d6047d85481d
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-09 04:33:49 +0200

    u

--- src/App.js
+++ src/App.js
@@ -2,30 +2,30 @@ import './index.css'
 import './App.css';
 import { Header } from 'components/Header/Header';
 import { Hero } from 'components/Hero/Hero';
-// import { useEffect } from 'react';
-// import { useState } from 'react';
-// import { useRef } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useRef } from 'react';
 
 //-----------------------------------------------------------------------------------
 export function App() {
-  // const [yOffset, SetYOffset] = useState(0)
+  const [yOffset, SetYOffset] = useState(0)
   // const myRef =useRef()
 
 
 
 
-  // const onScroll = (e) => {
-  //   SetYOffset(e.currentTarget.pageYOffset)
-  //   console.log(e);
-  // }
+  const onScroll = (e) => {
+    SetYOffset(e.currentTarget.pageYOffset)
+    console.log(e);
+  }
 
-  // useEffect(() => {
-  //   window.addEventListener('scroll', onScroll)
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll)
 
-  //   return () => (window.removeEventListener('scroll', onScroll))
+    return () => (window.removeEventListener('scroll', onScroll))
 
-  // }, [yOffset]);
-  // onWheel={onScroll}
+  }, [yOffset]);
+  
   return (
     <div className='backdrop' >
       <Header />
