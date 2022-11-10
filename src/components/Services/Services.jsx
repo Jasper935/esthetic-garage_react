d918f83608387dd3326a17e9d8320647641c95e3
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-10 23:30:04 +0200

    u

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,16 +1,30 @@
 import css from './Services.module.css';
 
 export const Services = () => {
-    const ServicesList =[{id:1, title: 'Detaling'},]
+  const ServicesList = [
+    { id: 1, title: 'Detaling' },
+    { id: 1, title: 'Detaling' },
+    { id: 1, title: 'Detaling' },
+    { id: 1, title: 'Detaling' },
+  ];
   return (
     <section className={css.services}>
       <div className={css.titleWrap}>
         <h2 className={css.title}>Послуги</h2>
       </div>
       <ul className={css.servicesLsit}>
-        <li className={css.servicesItem}>
-            <div className={css.ItemVisible}></div>
-        </li>
+        {ServicesList.map(({ title, id }) => {
+          return (
+            <li className={css.servicesItem}>
+              <div className={css.ItemVisible}>
+            <h4 className={css.ItemVisibleTitle}>{title}</h4>
+
+              </div>
+
+            </li>
+          );
+        })}
+
         <li className={css.servicesItem}></li>
         <li className={css.servicesItem}></li>
         <li className={css.servicesItem}></li>
