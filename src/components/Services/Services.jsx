211d81f7b303e6df996723993821de60bd121cc9
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-10 22:36:00 +0200

    u

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,16 +1,17 @@
-import css from './Services.module.css'
+import css from './Services.module.css';
 
-
-export const Services=()=>{
-
-    return(
-        <section className={css.services}>
-          <div className={css.titleWrap}>
-            <h2 className={css.title}>
-                Послуги
-            </h2>
-            </div> 
-            <div className={css.content}></div> 
-        </section>
-    )
-}
+export const Services = () => {
+  return (
+    <section className={css.services}>
+      <div className={css.titleWrap}>
+        <h2 className={css.title}>Послуги</h2>
+      </div>
+      <ul className={css.servicesLsit}>
+        <li className={css.servicesItem}></li>
+        <li className={css.servicesItem}></li>
+        <li className={css.servicesItem}></li>
+        <li className={css.servicesItem}></li>
+      </ul>
+    </section>
+  );
+};
