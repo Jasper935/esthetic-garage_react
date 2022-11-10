1f6e9261884b8aa5a14e1eab0c40440c76f705c8
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-10 22:53:46 +0200

    e

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,13 +1,16 @@
 import css from './Services.module.css';
 
 export const Services = () => {
+    const ServicesList =[{id:1, title: 'Detaling'},]
   return (
     <section className={css.services}>
       <div className={css.titleWrap}>
         <h2 className={css.title}>Послуги</h2>
       </div>
       <ul className={css.servicesLsit}>
-        <li className={css.servicesItem}></li>
+        <li className={css.servicesItem}>
+            <div className={css.ItemVisible}></div>
+        </li>
         <li className={css.servicesItem}></li>
         <li className={css.servicesItem}></li>
         <li className={css.servicesItem}></li>
