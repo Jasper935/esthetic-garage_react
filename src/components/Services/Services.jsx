4660b2d0fc36665d88e3b5e7ad68d4d735859b04
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-10 22:20:44 +0200

    U

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -5,7 +5,12 @@ export const Services=()=>{
 
     return(
         <section className={css.services}>
-            
+          <div className={css.titleWrap}>
+            <h2 className={css.title}>
+                Послуги
+            </h2>
+            </div> 
+            <div className={css.content}></div> 
         </section>
     )
 }
