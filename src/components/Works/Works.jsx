f4d3780be6d957b1ff958db8e7346621ca42b176
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-10 19:07:40 +0200

    u

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,50 +1,64 @@
 import css from './Works.module.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import {useSwiper, Swiper, SwiperSlide  } from 'swiper/react';
 import { worksPhotos } from 'worksList';
-// Import Swiper styles
+import svg from '../../images/symbol-defs.svg';
 import 'swiper/css';
 
 import 'swiper/css/pagination';
-import 'swiper/css/navigation';
 
-import { Autoplay, Pagination, Navigation } from 'swiper';
+
+import { Autoplay, Pagination } from 'swiper';
+import { useRef } from 'react';
 // import { useState } from 'react';
 export const Works = () => {
   // const [thumbsSwiper, setThumbsSwiper] = useState({})
-
+  const swiperRef = useRef();
+  
+  console.log(useSwiper());
   return (
     <section className={css.works}>
       <h2 className={css.worksTitle}>Наші роботи</h2>
       <div className={css.swiperWrap}>
-      <Swiper
-        className={css.swiper}
-        style={{
-          '--swiper-navigation-color': '#fff',
-          '--swiper-pagination-color': '#fff',
-          "--swiper-button-next-color":'#fff',
-          
-        }}
-        spaceBetween={100}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-          
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-      >
-        {worksPhotos.map((el, i) => {
-          return (
-            <SwiperSlide className={css.swiperSlide} key={i}>
-              <img src={el} alt={'work' + i} />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+      <button className={css.swiperBtnPrev} onClick={() => swiperRef.current.slidePrev()}>
+      <svg className={css.swiperBtnPrevIcon}>
+                      <use href={`${svg}#icon-arrow`} />
+                    </svg>
+      </button>
+      <button className={css.swiperBtnNext} onClick={() => swiperRef.current.slideNext()}>
+      <svg className={css.swiperBtnNextIcon}>
+                      <use href={`${svg}#icon-arrow`} />
+                    </svg>
+      </button>
+        <Swiper
+          className={css.swiper}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          style={{
+            '--swiper-navigation-color': '#fff',
+            '--swiper-pagination-color': '#fff',
+          }}
+          spaceBetween={100}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination]}
+        >
+         
+          {worksPhotos.map((el, i) => {
+            return (
+              <SwiperSlide className={css.swiperSlide} key={i}>
+                <img src={el} alt={'work' + i} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </section>
   );
