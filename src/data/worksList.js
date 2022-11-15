e98e2276db9c2235ccab742af0ced7a9d7c9cb23
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-15 18:42:02 +0200

    u

--- src/data/worksList.js
+++ src/data/worksList.js
@@ -1,9 +1,59 @@
-// import ph1 from '../images/works/IMG_8490.jpg';
-// import ph2 from '../images/works/IMG_8961.jpg';
-import ph3 from '../images/works/P1040689.jpg';
-import ph4 from '../images/works/P1120544.jpg';
-import ph5 from '../images/works/P1120643.jpg';
-import ph6 from '../images/works/P1120645.jpg';
-import ph7 from '../images/works/P1120680.jpg';
-import ph8 from '../images/works/P1120691.jpg';
-export const worksPhotos = [ ph3, ph4, ph5, ph6, ph7, ph8];
+import ph1 from '../images/works/IMG_8500.jpg';
+import ph2 from '../images/works/IMG_8517.jpg';
+import ph3 from '../images/works/IMG_8961.jpg';
+import ph4 from '../images/works/P1040639.jpg';
+import ph5 from '../images/works/P1040656.jpg';
+import ph6 from '../images/works/P1040668.jpg';
+import ph7 from '../images/works/P1040689.jpg';
+import ph8 from '../images/works/P1040715.jpg';
+import ph9 from '../images/works/P1040748.jpg';
+import ph10 from '../images/works/P1040755.jpg';
+import ph11 from '../images/works/P1040759.jpg';
+import ph12 from '../images/works/P1040775.jpg';
+import ph13 from '../images/works/P1040789.jpg';
+import ph14 from '../images/works/P1040852.jpg';
+import ph15 from '../images/works/P1040861.jpg';
+import ph16 from '../images/works/P1040870.jpg';
+import ph17 from '../images/works/P1040877.jpg';
+import ph18 from '../images/works/P1040879.jpg';
+import ph19 from '../images/works/P1040880.jpg';
+import ph20 from '../images/works/P1040888.jpg';
+import ph21 from '../images/works/P1040893.jpg';
+import ph22 from '../images/works/P1040904.jpg';
+import ph23 from '../images/works/P1040906.jpg';
+import ph24 from '../images/works/P1040910.jpg';
+import ph25 from '../images/works/P1040913.jpg';
+import ph26 from '../images/works/P1040917.jpg';
+import ph27 from '../images/works/P1040920.jpg';
+import ph28 from '../images/works/P1040921.jpg';
+import ph29 from '../images/works/P1040927.jpg';
+import ph30 from '../images/works/P1040930.jpg';
+import ph31 from '../images/works/P1040935.jpg';
+
+import ph32 from '../images/works/P1040939.jpg';
+import ph33 from '../images/works/P1040941.jpg';
+import ph34 from '../images/works/P1040944.jpg';
+
+export const worksPhotos = [ ph3, ph4, ph5, ph6, ph7, ph8, ph9,
+    ph10,
+    ph11,
+    ph12,
+    ph13,
+    ph14,
+    ph15,
+    ph16,
+    ph17,
+    ph18,
+    ph19,
+    ph20,
+    ph21,
+    ph22,
+    ph23,
+    ph24,
+    ph25,
+    ph26,
+    ph27,
+    ph28,
+    ph29,
+    ph30,
+    ph31];
