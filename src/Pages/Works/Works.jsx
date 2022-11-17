9a0e505db537fc9ff9b38bbe0c5d97416d2ad346
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-17 23:04:15 +0200

    u

--- src/Pages/Works/Works.jsx
+++ src/Pages/Works/Works.jsx
@@ -9,67 +9,76 @@ import { motion } from 'framer-motion';
 import { Autoplay, Pagination } from 'swiper';
 import { useRef } from 'react';
 import { Socials } from 'components/Socials/Socials';
+import logo from '../../images/logo2.png'
 // import { useState } from 'react';
 export const Works = () => {
   const swiperRef = useRef();
-  const inital ={opacity:0, }
-  const animate ={opacity:1 , }
-  const transition ={duration:2}
+  const inital = { opacity: 0 };
+  const animate = { opacity: 1 };
+  const transition = { duration: 2 };
   return (
-    <><Header text='Про нас' activeId={3} location='/about'/>
-    <motion.section className={css.works} initial={inital} animate={animate} transition={transition}>
-      
-      <h2 className={css.worksTitle}>Виконані роботи</h2>
-      <div className={css.swiperWrap}>
-        <button
-          className={css.swiperBtnPrev}
-          onClick={() => swiperRef.current.slidePrev()}
-        >
-          <svg className={css.swiperBtnPrevIcon}>
-            <use href={`${svg}#icon-arrow`} />
-          </svg>
-        </button>
-        <button
-          className={css.swiperBtnNext}
-          onClick={() => swiperRef.current.slideNext()}
-        >
-          <svg className={css.swiperBtnNextIcon}>
-            <use href={`${svg}#icon-arrow`} />
-          </svg>
-        </button>
-        <div className={css.worksSocials}>
-         <p className={css.worksSocialsTitle}>Це більше фото та відео контенту в наших соціальних мережах:</p> 
-         <Socials/>
+    <>
+      <Header text="Про нас" activeId={3} location="/about" />
+      <motion.section
+        className={css.works}
+        initial={inital}
+        animate={animate}
+        transition={transition}
+      >
+        <h2 className={css.worksTitle}>Виконані роботи</h2>
+        <div className={css.swiperWrap}>
+          {window.screen.width<767&&<img className={css.logo} src={logo} alt="logo" />}
+          <button
+            className={css.swiperBtnPrev}
+            onClick={() => swiperRef.current.slidePrev()}
+          >
+            <svg className={css.swiperBtnPrevIcon}>
+              <use href={`${svg}#icon-arrow`} />
+            </svg>
+          </button>
+          <button
+            className={css.swiperBtnNext}
+            onClick={() => swiperRef.current.slideNext()}
+          >
+            <svg className={css.swiperBtnNextIcon}>
+              <use href={`${svg}#icon-arrow`} />
+            </svg>
+          </button>
+          <div className={css.worksSocials}>
+            <p className={css.worksSocialsTitle}>
+              Це більше фото та відео контенту в наших соціальних мережах:
+            </p>
+            <Socials />
+          </div>
+          <Swiper
+            className={css.swiper}
+            onSwiper={swiper => {
+              swiperRef.current = swiper;
+            }}
+            style={{
+              '--swiper-pagination-color': '#fff',
+            }}
+            spaceBetween={100}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: true,
+            }}
+            // pagination={}
+            navigation={true}
+            modules={[Autoplay, Pagination]}
+            loop
+          >
+            {worksPhotos.map((el, i) => {
+              return (
+                <SwiperSlide className={css.swiperSlide} key={i}>
+                  <img src={el} alt={'work' + i} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         </div>
-        <Swiper
-          className={css.swiper}
-          onSwiper={swiper => {
-            swiperRef.current = swiper;
-          }}
-          style={{
-            '--swiper-pagination-color': '#fff',
-          }}
-          spaceBetween={100}
-          centeredSlides={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: true,
-          }}
-          // pagination={}
-          navigation={true}
-          modules={[Autoplay, Pagination]}
-          loop
-        >
-          {worksPhotos.map((el, i) => {
-            return (
-              <SwiperSlide className={css.swiperSlide} key={i}>
-                <img src={el} alt={'work' + i} />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </div>
-    </motion.section>
+      </motion.section>
     </>
   );
 };
