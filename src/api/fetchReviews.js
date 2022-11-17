a2476c2e276527e36b4dfabe16861f0bb7ef270c
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-18 01:45:55 +0200

    u

--- src/api/fetchReviews.js
+++ src/api/fetchReviews.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = 'https://6374203008104a9c5f7ac2e0.mockapi.io'
+axios.defaults.baseURL = 'https://6376b71081a568fc2504f4d3.mockapi.io'
 export async function fetchReviews() {
     try {
         const { data } = await axios.get('/reviews')
